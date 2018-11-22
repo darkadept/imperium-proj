af5adb3fb88a8eec10e53e5b4b94aacd9b6e2fed
darkadept <darkadept@durbn.net>
2018-11-22 11:49:49 -0600

    * Updated packages * Updated code to reflect new @thx/controls library

--- src/imperium/clientModules.js
+++ src/imperium/clientModules.js
@@ -1,6 +1,6 @@
 import graphql from '@imperium/graphql/lib/client';
 import auth from '@imperium/auth/client';
-import '@thx/date-ui/lib/styles.css';
+import '@thx/controls/lib/styles.css';
 import '../sample/client/sample.css';
 
 export default [
