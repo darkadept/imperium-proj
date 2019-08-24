b0c6c7c5aa2669768d53216d5733a92e4858e6ee
darkadept <darkadept@durbn.net>
2019-08-24 01:06:13 -0500

    testing

--- packages/web/src/core/client/index.tsx
+++ packages/web/src/core/client/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
+import client from '@imperium/core/lib/client';
 
+client();
 render(
 	<div>
 		hello world
--- packages/web/src/core/server/index.ts
+++ packages/web/src/core/server/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import server from '@imperium/core';
 import blah from './blah';
 
 export default function() {
+	server();
+
 	// Create express app
 	const app = express();
-	app.listen(process.env.PORT || 4000, () => {
+	app.listen(process.env.PORT || 4001, () => {
 		// console.log(`  PID ${process.pid} listening on port ${process.env.PORT || 4000}`);
 	});
 
