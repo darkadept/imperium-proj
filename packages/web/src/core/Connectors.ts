ca4f506b3c7d96da5ae78e1fe64655455686d853
darkadept <darkadept@durbn.net>
2019-08-28 15:39:05 -0500

    lint changes

--- packages/web/src/core/Connectors.ts
+++ packages/web/src/core/Connectors.ts
@@ -3,15 +3,16 @@ import {ImperiumConnectors} from '@imperium/core';
 
 export default class Connectors implements ImperiumConnectors {
 	async create() {
-		const mongoose = await connect(process.env.MONGOURL, {useNewUrlParser: true});
+		const mongoose = await connect(
+			process.env.MONGOURL,
+			{useNewUrlParser: true},
+		);
 
-		return {
-			mongo: mongoose.connection.db,
-			mongoose,
-		};
+		return {mongo: mongoose.connection.db, mongoose};
 	}
 
 	async close() {
+		// eslint-disable-next-line no-console
 		console.log('Closing web connectors');
 	}
 }
