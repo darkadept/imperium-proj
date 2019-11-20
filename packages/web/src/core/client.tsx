1208df7816d8048684373435ce12a526d353d7c2
JohnnySpomer <jonispom@gmail.com>
2019-11-20 13:52:31 -0600

    Added the THR4 Layout

--- packages/web/src/core/client.tsx
+++ packages/web/src/core/client.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import debug from 'debug';
+import 'semantic-ui-css/semantic.min.css';
 import ImperiumClient from '@imperium/client';
 import {RouteDirector} from '@imperium/router';
 import clientModules from './clientModules';
 import DefaultComponent from './DefaultComponent';
+import THR4Layout from '../THR4Layout/THR4Layout';
+import HeaderBar from '../THR4Layout/HeaderBar';
+import SideMenu from '../THR4Layout/SideMenu';
+import FooterBar from '../THR4Layout/FooterBar';
+import MenuBar from '../THR4Layout/MenuBar';
+import '../THR4Layout/styles.css';
 
 const d = debug('app.client');
 
@@ -12,7 +19,14 @@ const client = new ImperiumClient({
 	render(props) {
 		return (
 			<RouteDirector
-				routeDefaults={{redirect: false}}
+				routeDefaults={{
+					layout: THR4Layout,
+					menu: HeaderBar,
+					statusbar: MenuBar,
+					sidebar: SideMenu,
+					footer: FooterBar,
+					redirect: false,
+				}}
 				rootRoute={{path: '/', content: DefaultComponent, exact: true}}
 				{...props}
 			/>
